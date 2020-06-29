f857fbb965de9b2f60e8d4ab2a895058fd2cb83d
lazervabeman <ali.soueidan@outlook.de>
2020-06-29 19:27:45 +0200

    smaller improvements

---
+++ src/stories/Stories.vue
@@ -0,0 +1,53 @@
+<template lang="pug">
+
+  .story
+    h1 Some description about stories
+    p Just place your components within a storie component, within the stories folder.
+    p You can use all the vue functionality, included dependencies, libraries and so on.
+    p Storyvue will list your component stories within the sidebar and name it as like the related file.
+    p In future it will also visualize the folder structure as well.
+
+</template>
+
+
+<script>
+
+// Import Libraries
+import { mapActions, mapGetters } from 'vuex';
+
+// Components
+import Select from '@/components/Select.vue';
+
+export default {
+  components: {
+    Select,
+  },
+  computed: {
+    ...mapGetters([
+      'knobData',
+    ]),
+  },
+  data() {
+    return {
+    };
+  },
+  methods: {
+    ...mapActions([
+      'updateKnobsData',
+    ]),
+  },
+};
+
+</script>
+
+
+<style lang="sass" scoped>
+
+@import @/styles/base
+
+#signup
+  display: grid
+  grid-gap: gap(20)
+  padding: gap(20)
+
+</style>
---
+++ src/stories/atoms/Atoms.vue
@@ -0,0 +1,53 @@
+<template lang="pug">
+
+  .stories
+    h1 Some description about atoms
+    p If atoms are the basic building blocks of matter, then the atoms of our interfaces serve as the foundational
+    p building blocks that comprise all our user interfaces. These atoms include basic HTML elements like form labels,
+    p inputs, buttons, and others that can’t be broken down any further without ceasing to be functional.
+    a(href="https://atomicdesign.bradfrost.com/chapter-2/" target="_blank" rel="noopener") atomicdesign by bradfrost.
+
+</template>
+
+
+<script>
+
+// Import Libraries
+import { mapActions, mapGetters } from 'vuex';
+
+// Components
+import Select from '@/components/Select.vue';
+
+export default {
+  components: {
+    Select,
+  },
+  computed: {
+    ...mapGetters([
+      'knobData',
+    ]),
+  },
+  data() {
+    return {
+    };
+  },
+  methods: {
+    ...mapActions([
+      'updateKnobsData',
+    ]),
+  },
+};
+
+</script>
+
+
+<style lang="sass" scoped>
+
+@import @/styles/base
+
+#signup
+  display: grid
+  grid-gap: gap(20)
+  padding: gap(20)
+
+</style>
